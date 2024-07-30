c7978b9f4ca1289b46260e10eeeeae9155d5a66a
Andrey Zommer <zommer.02@mail.ru>
2024-07-30 18:14:04 +0300

    add alпgorithm аÑfor working with db

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Зоммер", "Андрей", (byte) 22);
+        userService.saveUser("Лев", "Толстой", (byte) 82);
+        userService.saveUser("Райан", "Гослинг", (byte) 43);
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
